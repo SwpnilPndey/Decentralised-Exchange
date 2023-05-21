bb5d0d0767f941260be5d29bd6ca49f12e0e466d
SwpnilPndey <Swpnil.Pndey@gmail.com>
2023-05-21 11:39:32 +0530

    Initial commit

---
+++ scripts/deploy.js
@@ -0,0 +1,18 @@
+const hre = require("hardhat");
+
+async function main() {
+  
+  const DEX = await hre.ethers.getContractFactory("DEX");
+  const dex = await DEX.deploy();
+
+  await dex.deployed();
+
+  console.log(`Contract deployed to ${dex.address}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
